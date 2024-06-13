5e641bf808476608b479f1f519fc52c5b7fe6ecb
Marcos Parreiras <marcosalp1998@gmail.com>
2024-06-13 13:21:09 -0300

    init: bootstrap a client-side application with create-react-admin

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import {
+  Admin,
+  Resource,
+  ListGuesser,
+  EditGuesser,
+  ShowGuesser,
+} from "react-admin";
+import { dataProvider } from "./dataProvider";
+
+export const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="users"
+      list={ListGuesser}
+      edit={EditGuesser}
+      show={ShowGuesser}
+    />
+  </Admin>
+);
